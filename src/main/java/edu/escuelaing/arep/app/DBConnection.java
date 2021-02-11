d2a06f8c68cca97062f8764c10aa823d4cba66b7
Jose Gutierrez Marin <andres.gutierrez-m@mail.escuelaing.edu.co>
2021-02-11 15:23:21 -0500

    Conexion db

--- src/main/java/edu/escuelaing/arep/app/DBConnection.java
+++ src/main/java/edu/escuelaing/arep/app/DBConnection.java
@@ -8,10 +8,10 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class DBConnection {
-	private static String uri = "jdbc:postgresql://lyydybmfpdsoyf:17e675c3cf6ae9399f94b245f8f1b28d2b572e58e9a2f3a0a310722e86811cec@ec2-3-211-245-154.compute-1.amazonaws.com:5432/d1s8lu3t47f6cm";
+	private static String uri = "jdbc:postgresql://ec2-3-211-245-154.compute-1.amazonaws.com:5432/d1s8lu3t47f6cm?ssl=true&sslmode=require";
 	private static String user = "lyydybmfpdsoyf";
 	private static String password = "17e675c3cf6ae9399f94b245f8f1b28d2b572e58e9a2f3a0a310722e86811cec";
-	private static Connection c;
+	private static Connection c = null;
 	
 	/**
 	 * Constructor de la Clase.
--- src/main/java/edu/escuelaing/arep/app/httpServer.java
+++ src/main/java/edu/escuelaing/arep/app/httpServer.java
@@ -67,7 +67,7 @@ public class httpServer {
 		if (System.getenv("PORT") != null) {
 			return Integer.parseInt(System.getenv("PORT"));
 		}
-		return 35000; //returns default port if heroku-port isn't set(i.e. on localhost)
+		return 35001; //returns default port if heroku-port isn't set(i.e. on localhost)
 	}
 	
 	
